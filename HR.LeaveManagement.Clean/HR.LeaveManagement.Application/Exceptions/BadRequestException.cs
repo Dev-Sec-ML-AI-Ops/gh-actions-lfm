182693c90e480b4971076fcf51ccd2fa76e73c79
Anil Kumar <anilnishad298@gmail.com>
2026-03-31 21:28:50 +0530

    Added HR-Leave Management Project as well CI workflow

---
+++ HR.LeaveManagement.Clean/HR.LeaveManagement.Application/Exceptions/BadRequestException.cs
@@ -0,0 +1,31 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace HR.LeaveManagement.Application.Exceptions;
+
+public class BadRequestException : Exception
+{
+    public BadRequestException(string message) : base(message)
+    {
+
+    }
+
+    //public BadRequestException(string message, FluentValidation.Results.ValidationResult validationResult) : this(message)
+    //{
+    //}
+
+    //public BadRequestException(string message, FluentValidation.Results.ValidationResult validationResult) : base(message)
+    //{
+    //    ValidationErrors = new();
+    //    foreach (var error in validationResult.Errors)
+    //    {
+    //        ValidationErrors.Add(error.ErrorMessage);
+    //    }
+    //}
+
+    //public List<string> ValidationErrors { get; set; }
+    public BadRequestException(string message, FluentValidation.Results.ValidationResult validationResult) : base(message)
+    {
+        ValidationErrors = validationResult.ToDictionary();
+    }
+    public IDictionary<string, string[]> ValidationErrors { get; set; }
+}
